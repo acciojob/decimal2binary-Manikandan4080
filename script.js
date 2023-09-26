1cfa2f15dcbc6bca34e6f063368c8c205e940136
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-09-26 21:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+	let ans = ""
+	while(num > 0){
+		if(num & 1 == 1){
+			ans = "1" + ans
+		}
+		else{
+			ans = "0" + ans
+		}
+		num = num>>1
+	}
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
